fab8da50e55aed4da91c82fca53072ec73391173
James Penera <jamespenera@icloud.com>
2024-06-06 19:45:05 +0400

    nodemon
    npm install --save-dev nodemon

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = requre('express');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
