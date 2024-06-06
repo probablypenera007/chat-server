8c76d3be6180ec4559d502bddf7865665f980dc8
James Penera <jamespenera@icloud.com>
2024-06-07 00:42:04 +0400

    loggers set, setting joi and celebrate for  validation

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const routes = require("./routes")
 const { PORT = 3001 , MONGODB_URI} = require("./utils/config");
 const { createChatUser, login } = require("./controllers/chatUserController");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 
 const app = express();
@@ -23,11 +25,15 @@ mongoose.connect(MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 
+app.use(requestLogger)
+
 app.post("/signin", login)
 app.post("/signup", createChatUser)
 
 app.use(routes);
 
+app.use(errorLogger)
+app.use(errorHandler);
 
 app.listen((PORT)
   , () => {
