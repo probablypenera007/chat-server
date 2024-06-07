be9df09d1d8c93347474260437f0132446cf8437
James Penera <jamespenera@icloud.com>
2024-06-07 13:15:06 +0400

    Update message.test.js

--- tests/message.test.js
+++ tests/message.test.js
@@ -23,5 +23,22 @@ beforeAll(async () => {
   });
   
   describe('Message Model Test', () => {
-
+    it('should create a message with required fields', async () => {
+        const sender = await ChatUser.findOne({ username: 'sender123' });
+        const receiver = await ChatUser.findOne({ username: 'receiver123' });
+    
+        const message = new Message({
+          sender: sender._id,
+          receiver: receiver._id,
+          message: 'Hello!',
+        });
+        const savedMessage = await message.save();
+    
+        expect(savedMessage._id).toBeDefined();
+        expect(savedMessage.sender).toBe(sender._id);
+        expect(savedMessage.receiver).toBe(receiver._id);
+        expect(savedMessage.message).toBe('Hello!');
+        expect(savedMessage.messageStatus).toBe('sent');
+        expect(savedMessage.createdAt).toBeDefined();
+      });
   });
