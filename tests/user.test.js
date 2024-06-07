233bc4e74b907ae0b1eb9594feb1205ca7c583ac
James Penera <jamespenera@icloud.com>
2024-06-07 12:22:27 +0400

    createuser model test pass

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,3 +1,30 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const ChatUser = require('../models/user');
 
+const { connect, closeDatabase } = require('./fixtures/db');
+
+beforeAll(async () => {
+    await connect();
+  });
+  
+  afterAll(async () => {
+    await closeDatabase();
+  });
+
+  describe('ChatUser Model Test', () => {
+    it('should create a user with required fields', async () => {
+      const user = new ChatUser({
+        username: 'testuser12345',
+        password: await bcrypt.hash('password', 10),
+      });
+      const savedUser = await user.save();
+  
+      expect(savedUser._id).toBeDefined();
+      expect(savedUser.username).toBe('testuser12345');
+      expect(savedUser.password).toBeDefined();
+    });
+
+
+
+});
